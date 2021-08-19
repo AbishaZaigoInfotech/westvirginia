42ef3c851b22678ad6254e363ab46b0d3f35212a
Abishazaigo <you@example.com>
2021-08-19 17:22:37 +0530

    station count in dashboard

--- resources/views/vendor/larasnap/dashboard.blade.php
+++ resources/views/vendor/larasnap/dashboard.blade.php
@@ -28,28 +28,6 @@
                 </div>
             </div>
         </div>
-        <!-- <div class="col-lg-3 col-sm-6">
-            <div class="card-box bg-blue">
-                <div class="inner">
-                    <h3> {{ $rolesCount }} </h3>
-                    <h5> Roles </h5>
-                </div>
-                <div class="icon">
-                    <i class="fa fa-lock" aria-hidden="true"></i>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-3 col-sm-6">
-            <div class="card-box bg-orange">
-                <div class="inner">
-                    <h3> {{ $screensCount }} </h3>
-                    <h5> Screens </h5>
-                </div>
-                <div class="icon">
-                    <i class="fa fa-desktop"></i>
-                </div>
-            </div>
-        </div> -->
     </div>
 </div>
 <!-- Page Content End-->				  
