e89e858d5034f7e2720df30bf4ebf11f362469eb
Abishazaigo <you@example.com>
2021-08-24 11:50:33 +0530

    Station active and inactive count

--- resources/views/vendor/larasnap/dashboard.blade.php
+++ resources/views/vendor/larasnap/dashboard.blade.php
@@ -9,6 +9,7 @@
         <div class="col-lg-3 col-sm-6">
             <div class="card-box bg-green"> 
                 <div class="inner">
+                    <?php $stationsActiveCount   = App\Models\Station::where('status', 1)->count(); ?>
                     <h3>{{$stationsActiveCount}}</h3>
                     <h5> Active Stations </h5>
                 </div>
@@ -20,7 +21,11 @@
         <div class="col-lg-3 col-sm-6">
             <div class="card-box bg-red">
                 <div class="inner">
+                @if(isset($stationsInactiveCount) && $stationsInactiveCount)
                     <h3>{{$stationsInactiveCount}}</h3>
+                    @else
+                    <h3>0</h3>
+                    @endif
                     <h5> Inactive Stations </h5>
                 </div>
                 <div class="icon">
