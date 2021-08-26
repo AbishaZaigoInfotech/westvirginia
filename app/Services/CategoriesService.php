ee0ce80b82ef35784f2765e9539f49a6982d5c70
Abishazaigo <you@example.com>
2021-08-26 17:16:06 +0530

    Listing Categories

--- app/Services/CategoriesService.php
+++ app/Services/CategoriesService.php
@@ -10,7 +10,8 @@ class CategoriesService
     public function index(Request $request)
     {
         try{
-            $categories = Category::select('id', 'name', 'label')->get();
+            $format = Category::where('name','categories')->select('id')->first();
+            $categories = Category::where('parent_category_id', $format['id'])->select('id', 'name', 'label')->get();
             return $categories;
         } catch (\Exception $e) {
             return apiResponse("Categories are not listed", 400, (object)[]);
