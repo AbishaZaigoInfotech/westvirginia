b9b6c42ca772459242e9446cf79425e0e821125c
Abishazaigo <you@example.com>
2021-09-13 12:38:38 +0530

    Added category collection

--- app/Http/Controllers/API/CategoryController.php
+++ app/Http/Controllers/API/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Services\CategoriesService;
+use App\Http\Resources\CategoriesCollection;
 
 class CategoryController extends Controller
 {
@@ -16,6 +17,9 @@ class CategoryController extends Controller
     public function index(Request $request)
     {
         $categories = $this->categoriesService->index($request);
-        return apiResponse("Categories listed successfully", 200, $categories);
+        if($categories){
+            $categories = CategoriesCollection::collection($categories);
+            return apiResponse("Categories listed successfully", 200, $categories);
+        }
     }
 }
