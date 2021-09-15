297e90e6d69113abeccd327fa8a43b3f785c6fa2
Abishazaigo <you@example.com>
2021-09-15 14:04:07 +0530

    added social media and notification list

---
+++ database/migrations/2021_09_15_081407_alter_sociallinks_change_to_stations.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterSociallinksChangeToStations extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('stations', function (Blueprint $table) {
+            //
+            $table->string('facebook')->nullable()->change();
+            $table->string('twitter')->nullable()->change();
+            $table->string('instagram')->nullable()->change();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('stations', function (Blueprint $table) {
+            //
+        });
+    }
+}
